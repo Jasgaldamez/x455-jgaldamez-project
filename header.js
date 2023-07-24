9815f6b5295885d3eecb954f55ba507a079ec10a
Jasgaldamez <jasmine.galdamez.995@gmail.com>
2023-07-24 09:40:21 -0700

    Added javascript for header.

---
+++ header.js
@@ -0,0 +1,18 @@
+document.getElementById("nav").innerHTML = `
+    <header class="topnav">
+        <a href="./home.html">
+            <img
+                class="home"
+                src="./assets/images/logo.png"
+                width="202"
+                height="115"
+            ></img>
+        </a>
+        <div class="menu">
+            <a href="./adoptAnimals.html">Adopt</a>
+            <a href="#about-us">About Us</a>
+            <a href="#get-involved">Get Involved</a>
+            <button class="donate-button">Donate</button>
+        </div>
+    </header>
+`;
