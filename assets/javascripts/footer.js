2a0e8ad3fc16960b4fe272c0928abebfac77cb56
Jasgaldamez <jasmine.galdamez.995@gmail.com>
2023-07-24 17:05:28 -0700

    added footer

---
+++ assets/javascripts/footer.js
@@ -0,0 +1,55 @@
+document.getElementById("footer").innerHTML = `<div class="footer">
+  <div class="bottom-nav">
+    <a>Adopt</a>
+    <a>About Us</a>
+    <a>Get Involved</a>
+  </div>
+  <div class="company">
+    <div class="company-content">
+      <div class="company-labels">Hours of Operation</div>
+      <div>Open daily by appointment</div>
+    </div>
+    <div class="company-content">
+      <div class="company-labels">Contact Us</div>
+      <div>(530)894-5630</div>
+    </div>
+    <div class="company-content">
+      <div class="company-labels">Address</div>
+      <div>2579 Fair St Chico, CA 95928</div>
+    </div>
+  </div>
+  <div class="newsletter">
+    <div class="newsletter-input-container">
+      <label for="newslettter">Sign up for newsletter</label>
+      <input
+        class="newsletter-input"
+        type="text"
+        placeholder="Email"
+        id="newsletter"
+      />
+
+      <div class="social-media">
+        <a href="https://www.facebook.com/ChicoAnimalShelter/" target="_blank">
+          <img src="./assets/images/Facebook 2.svg" alt="facebook" />
+        </a>
+        <a
+          href="https://www.instagram.com/chicoanimalshelter/?hl=en"
+          target="_blank"
+        >
+          <img src="./assets/images/instagram.svg" alt="facebook" />
+        </a>
+        <a
+          href="https://www.amazon.com/gp/registry/wishlist/Z38M6M4QKB74/ref=cm_wl_list_o_0"
+          target="_blank"
+        >
+          <img src="./assets/images/amazon.svg" alt="facebook" />
+        </a>
+      </div>
+    </div>
+
+    <div class="sign-up">
+      <button class="sign-up-button">Sign-Up</button>
+    </div>
+  </div>
+</div>
+`;
